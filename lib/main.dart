ea31fd9071efe37999fee678cf4eda845b074b5c
zhangxianlei <1079105171@qq.com>
2024-02-28 15:37:08 +0800

    doc

--- lib/main.dart
+++ lib/main.dart
@@ -36,13 +36,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int counter = 0;
   String name = '';
   String token = '默认token';
 
   void _incrementCounter() {
     setState(() {
-      _counter++;
+      counter++;
     });
   }
 
@@ -69,10 +69,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      //   title: Text(widget.title),
+      // ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -81,7 +81,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '按了多少次:',
             ),
             Text(
-              '$_counter',
+              '$counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             Text(name),
